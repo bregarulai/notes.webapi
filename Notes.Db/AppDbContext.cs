88b6eb5c824805f8fa131f390266834e9070b4c6
bregarulai <bregarulai@gmail.com>
2021-10-10 14:06:08 -0400

    rest of project

---
+++ Notes.Db/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ Notes.WebApi/Controllers/NotesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Notes.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+       
+        private readonly ILogger<NotesController> _logger;
+
+        public NotesController(ILogger<NotesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult CreateNote()
+        {
+            return Ok("My first notes");
+        }
+    }
+}
